2679b37c65c199f248861264a289463c63868db3
pjehan <pierre.jehan@gmail.com>
2022-07-22 11:44:25 +0200

    Création du CRUD des genres

--- src/Form/GenreType.php
+++ src/Form/GenreType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Genre;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +12,7 @@ class GenreType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
-                'label' => 'Nom'
-            ])
+            ->add('name')
         ;
     }
 
